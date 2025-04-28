dfa2d1405092b7d24c671b8787060ead4da58d4f
Augusto Castilho Borges <augustocastilhoborges21@gmail.com>
2025-04-28 13:58:10 -0300

    fix

--- src/functions/synchronize-language-keys.js
+++ src/functions/synchronize-language-keys.js
@@ -12,9 +12,9 @@ export function synchronizeLanguageKeys(pt, en, es) {
   const synchronizeES = {};
 
   for (const key of sortedKeys) {
-    synchronizePT[key] = pt[key] || '[Tradução pendente para PT: ${key}]';
-    synchronizeEN[key] = en[key] || '[Missing translation for EN: ${key}]';
-    synchronizeES[key] = es[key] || '[Traducción pendiente para ES: ${key}]';
+    synchronizePT[key] = pt[key] || `[Tradução pendente para PT: ${key}]`;
+    synchronizeEN[key] = en[key] || `[Missing translation for EN: ${key}]`;
+    synchronizeES[key] = es[key] || `[Traducción pendiente para ES: ${key}]`;
   }
 
   return {
